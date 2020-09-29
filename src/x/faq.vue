e7343eea944c74e7cb287ac806a87b3ea1362a5e
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-29 08:04:22 +0100

    Update faq.vue

--- src/x/faq.vue
+++ src/x/faq.vue
@@ -55,6 +55,15 @@ export default {
           text:
             "THIRM is avaiable on DEXs, we recommend uniswap to buy and sell your thirm tokens.",
         },
+ {
+          title: "THIRM (THRM) Max Supply",
+          text:
+            "Supply is at 100,000 THIRM, Governance can mint more, if proposal to do so passes. ",
+        }, {
+          title: "How do i Vote ?",
+          text:
+            "Voting platform is under development, and will allow you to vote via a smart contract influencing every decision THIRM PROTOCOL Makes",
+        },
       ],
     };
   },

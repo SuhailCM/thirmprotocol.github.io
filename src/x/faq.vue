73efa0e55e73e8620c57c91bf543e36041891d44
Aman Singh <besoeasy@users.noreply.github.com>
2020-09-29 20:53:51 +0100

    Update faq.vue

--- src/x/faq.vue
+++ src/x/faq.vue
@@ -46,23 +46,46 @@ export default {
       isOpen: 0,
       collapses: [
         {
-          title: "How can i use THIRM ?",
+          title: "Where do I  buy Thirm ?",
           text:
-            "THIRM is Ethereum Token, which means its useable from any Ethereum wallet. We recommend Trust Wallet.",
+            "Thirm is avaiable on DEXs, we recommend uniswap to buy and sell your thirm tokens.",
         },
         {
-          title: "How can i trade THIRM ?",
+          title: "How does the leaderboard work ?",
           text:
-            "THIRM is avaiable on DEXs, we recommend uniswap to buy and sell your thirm tokens.",
+            "Leaderboard is based on the people providing liquidity on Uniswap, the more you provide the bigger part of the airdrop specified you will receive. The reward will be paid out at the end of the Beta and will be based on a random snapshot taken during the month.",
         },
- {
-          title: "THIRM (THRM) Max Supply",
+        {
+          title: "When is the end of the Beta?",
+          text:
+            "As with all development it is not easy to know when product is finished but the current plan is to have the product available at  1st week of December.",
+        },
+        {
+          title: "What is the THIRM (THRM) total supply ?",
           text:
             "Supply is at 100,000 THIRM, Governance can mint more, if proposal to do so passes. ",
-        }, {
-          title: "How do i Vote ?",
+        },
+
+        {
+          title: "Is there a liquidity lock?",
+          text:
+            "Yes, you can find further information on unicrypt https://unicrypt.network/uniswap-browser/pair/0x8ce44024c46545E5Ab1D16C326Ea157dad7B3c00  ",
+        },
+
+        {
+          title: "What is the Thirm token good for ?",
+          text:
+            "As a holder of Thirm you will have the right to vote on the governance of the project, in addition 0.5% of earnings from staking will be paid as fees which are used to buy Thirm from DEX and burn them in order to decrease the supply. ",
+        },
+        {
+          title: "How do i vote ?",
           text:
             "Voting platform is under development, and will allow you to vote via a smart contract influencing every decision THIRM PROTOCOL Makes",
+        },
+        {
+          title: "Will you do marketing?",
+          text:
+            "We have plans for marketing in the future, but currently the main focus is to release our product.",
         },
       ],
     };

2db0960bb6a5c2600c79b6a311673537ff94e3ca
Aman Singh <besoeasy@users.noreply.github.com>
2020-10-11 04:07:58 +0100

    Update faq.vue

--- src/x/faq.vue
+++ src/x/faq.vue
@@ -56,40 +56,22 @@ export default {
       isOpen: 0,
       collapses: [
         {
-          title: "Where do I  buy Thirm ?",
-          text:
-            "Thirm is avaiable on DEXs, we recommend uniswap to buy and sell your thirm tokens.",
+          title: "Getting THIRM Tokens",
+          text: "THIRM Tokens are not for sale by the team, only way to obtain THIRM Tokens is contacting/buying/trading from current holders. Uniswap Exchange is best place for you to find these traders.",
         },
         {
-          title: "When is the end of the Beta ?",
-          text:
-            "As with all development it is not easy to know when product is finished but the current plan is to have the product available at  1st week of December.",
+          title: "THIRM Tokens Supply",
+          text: "Max Supply is at 100,000 THIRM. Governance can mint more if needed. ",
         },
         {
-          title: "What is the THIRM (THRM) total supply ?",
-          text: "Max Supply is at 100,000 THIRM. ",
+          title: "THIRM Tokens Uses",
+          text: "Thirm Token is your governance share in Thirm Protocol, all holders decide future of Thirm Protocol.",
         },
-
-        {
-          title: "Is there a liquidity lock?",
-          text:
-            "Yes, you can find further information on unicrypt https://unicrypt.network/uniswap-browser/pair/0x8ce44024c46545E5Ab1D16C326Ea157dad7B3c00  ",
+ {
+          title: "THIRM Tokens Value",
+          text: "Thirm Token holds no value, Thirm Token is only used to govern/operate/manage Thirm Protocol, it has no real world value.",
         },
-        {
-          title: "How do i vote ?",
-          text:
-            "Voting platform is under development, and will allow you to vote via a smart contract influencing every decision THIRM PROTOCOL Makes",
-        },
-        {
-          title: "Will you do marketing?",
-          text:
-            "We have plans for marketing in the future, but currently the main focus is to release our product.",
-        },
-        {
-          title: "How does the leaderboard work ?",
-          text:
-            "Leaderboard is based on the people providing liquidity on Uniswap, the more you provide the bigger part of the airdrop specified you will receive. The reward will be paid out at the end of the Beta and will be based on a random snapshot taken during the month.",
-        },
+    
       ],
     };
   },

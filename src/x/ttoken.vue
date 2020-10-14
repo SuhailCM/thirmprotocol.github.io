dc47dfb6884e44712d6277a1fdd4fafd8672caa6
Aman Singh <besoeasy@users.noreply.github.com>
2020-10-14 07:00:02 +0100

    Update ttoken.vue

--- src/x/ttoken.vue
+++ src/x/ttoken.vue
@@ -73,31 +73,48 @@
 </template>
 
 <script>
-import axios from "axios";
-import _ from "lodash";
-
 export default {
   data() {
     return {
-      lot: [],
+      lot: [
+        {
+          name: "Thirm Ethereum",
+          symbol: "tETH",
+          address: "0x138083bd8a49357b4630733954e9538ed74df105",
+          logoURI: "https://thirm.com/icons/tETH.png",
+        },
+        {
+          name: "Thirm Nano",
+          symbol: "tNANO",
+          address: "0x8bd868574a92994e9c51e501523c255f561b34ad",
+          logoURI: "https://thirm.com/icons/tNANO.png",
+        },
+        {
+          name: "Thirm Tether",
+          symbol: "tUSDT",
+          address: "0xB962776Fa4FaD32F46609A813488C4b62227325f",
+          logoURI: "https://thirm.com/icons/tUSDT.png",
+        },
+        {
+          name: "Thirm Dai",
+          symbol: "tDAI",
+          address: "0xB29c13A70792B5B5D1Ab8e40b92233A16568Fe32",
+          logoURI: "https://thirm.com/icons/tDAI.png",
+        },
+        {
+          name: "Thirm Bitcoin",
+          symbol: "tBTC",
+          address: "0x36910c9695C1286c9377CEbc61D37bDEFcFdeAC6",
+          logoURI: "https://thirm.com/icons/tBTC.png",
+        },
+        {
+          name: "Thirm Chainlink",
+          symbol: "tLINK",
+          address: "0x36910c9695C1286c9377CEbc61D37bDEFcFdeAC6",
+          logoURI: "https://thirm.com/icons/tLINK.png",
+        },
+      ],
     };
-  },
-  mounted() {
-    axios({
-      method: "GET",
-      url: "https://thirm.com/tokens.json",
-    }).then((responsex) => {
-      var temparr = [];
-      _.forEach(responsex.data.tokens, function (value) {
-        console.log(value.chainId);
-
-        if (value.chainId == 1) {
-          temparr.push(value);
-        }
-      });
-
-      this.lot = temparr;
-    });
   },
 };
 </script>

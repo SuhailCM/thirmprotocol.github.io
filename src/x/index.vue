b657330be709b91961dff9e985f24e364ff1b2d3
Aman Singh <besoeasy@users.noreply.github.com>
2020-11-29 21:04:52 +0000

    Update index.vue

--- src/x/index.vue
+++ src/x/index.vue
@@ -8,33 +8,9 @@
 							<lottie-player src="https://thirm.com/animations/feat.json" background="transparent" speed="0.6" style="width: 100%; height: 100%" loop autoplay></lottie-player>
 						</div>
 						<div class="column is-offset-1">
+							<b-notification type="is-link is-light" aria-close-label="Close notification"> Approx 9000 THIRM is added on <a href="https://app.bounce.finance/fixed-swap/4608">bounce.finance</a>, 1 ETH = 40 THIRM. The generated ETH will be locked in Uniswap LP Permanently. </b-notification>
+
 							<div class="content box"><strong>THIRM VM</strong> links various cross-chain cryptocurrencies, allowing you to access your crypto assets on different blockchains.</div>
-
-							<br />
-							<div class="columns is-multiline">
-								<div class="column  is-4  zoom">
-									<a target="_blank" href="https://www.coingecko.com/en/coins/thirm-protocol"> <img src="../assets/tags/coingecko.png"/></a>
-								</div>
-
-								<div class="column  is-4  zoom">
-									<a target="_blank" href="https://coinmarketcap.com/currencies/thirm-protocol"> <img src="../assets/tags/coinmarketcap.png"/></a>
-								</div>
-								<div class="column  is-4  zoom">
-									<a target="_blank" href="https://www.livecoinwatch.com/price/Thirm-THIRM"> <img src="../assets/tags/livecoinwatch.png"/></a>
-								</div>
-								<div class="column is-4 zoom">
-									<a target="_blank" href="https://www.stateofthedapps.com/dapps/thirm">
-										<img src="../assets/tags/sod.png" />
-									</a>
-								</div>
-								<div class="column  is-4  zoom">
-									<a target="_blank" href="https://uniswap.info/token/0xb526fd41360c98929006f3bdcbd16d55de4b0069"> <img src="../assets/tags/uniswap.png"/></a>
-								</div>
-
-								<div class="column  is-4  zoom">
-									<a target="_blank" href="https://etherscan.io/token/0xb526fd41360c98929006f3bdcbd16d55de4b0069"> <img src="../assets/tags/etherscan.png"/></a>
-								</div>
-							</div>
 						</div>
 					</div>
 				</div>
@@ -43,14 +19,29 @@
 
 		<section>
 			<div class="container">
-				<b-notification type="is-white" aria-close-label="Close notification">
-					<div class="columns is-vcentered">
-						<div class="column">Approx 9000 THIRM is added on bounce.finance, 1 ETH = 40 THIRM. The generated ETH will be locked in Uniswap LP Permanently.</div>
-						<div class="column is-2">
-							<a href="https://app.bounce.finance/fixed-swap/4608">$THIRM on bounce.finance</a>
-						</div>
+				<div class="columns is-multiline">
+					<div class="column zoom">
+						<a target="_blank" href="https://www.stateofthedapps.com/dapps/thirm">
+							<img src="../assets/tags/sod.png" />
+						</a>
 					</div>
-				</b-notification>
+
+					<div class="column zoom">
+						<a target="_blank" href="https://www.coingecko.com/en/coins/thirm-protocol"> <img src="../assets/tags/coingecko.png"/></a>
+					</div>
+
+					<div class="column zoom">
+						<a target="_blank" href="https://coinmarketcap.com/currencies/thirm-protocol"> <img src="../assets/tags/coinmarketcap.png"/></a>
+					</div>
+
+					<div class="column zoom">
+						<a target="_blank" href="https://uniswap.info/token/0xb526fd41360c98929006f3bdcbd16d55de4b0069"> <img src="../assets/tags/uniswap.png"/></a>
+					</div>
+
+					<div class="column zoom">
+						<a target="_blank" href="https://etherscan.io/token/0xb526fd41360c98929006f3bdcbd16d55de4b0069"> <img src="../assets/tags/etherscan.png"/></a>
+					</div>
+				</div>
 			</div>
 		</section>
 

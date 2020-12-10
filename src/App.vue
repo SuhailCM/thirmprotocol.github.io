d2d6dc65b4f9f5c8cc9678d9dbc0798a8bd7681b
Aman Singh <besoeasy@users.noreply.github.com>
2020-12-10 10:39:05 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 					<b-navbar-item target="_blank" href="https://docs.thirm.com/">
 						<b-button>DOCS</b-button>
 					</b-navbar-item>
-                                        <b-navbar-item target="_blank" href="https://github.com/orgs/thirmprotocol/projects">
+                    <b-navbar-item target="_blank" href="https://github.com/orgs/thirmprotocol/projects">
 						<b-button>ROADMAP</b-button>
 					</b-navbar-item>
 					<b-navbar-item target="_blank" href="https://uniswap.info/token/0xb526fd41360c98929006f3bdcbd16d55de4b0069">

f2b6aaf956a9b52ff78f65585dd3c74cb3d5ecb9
l3lackcurtains <info@crumet.com>
2021-01-09 00:47:54 -0700

    modern timeline

--- src/x/roadmap.vue
+++ src/x/roadmap.vue
@@ -1,103 +1,323 @@
 <template>
-	<div>
+  <div>
     <section class="container">
       <div class="roadmap">
-        <ul class="roadmap-list" v-for="(roadmap, index) of roadmaps" :key="index">
-          <li>
-            <div class="roadmap-date">
-             {{ roadmap.date }}
-            </div>
-            <div class="roadmap-title">
-              {{ roadmap.title }}
-            </div>
-            <div class="roadmap-desc">
-              {{ roadmap.description }}
-            </div>
+        <ul class="timeline">
+        <li class="timeline" v-for="(roadmap, index) of roadmaps" :key="index">
+          <div :class="index % 2=== 0 ? 'direction-l' : 'direction-r'">
+            <div class="flag-wrapper">
+        <span class="flag">{{ roadmap.title }}</span>
+        <span class="time-wrapper"><span class="time">{{ roadmap.date }}</span></span>
+        </div>
+        <div class="desc">{{ roadmap.description }}</div>
+        </div>
           </li>
-        </ul>
-        <h1 class="upcom">Upcoming</h1>
-        <ul class="roadmap-list upcomming" v-for="(roadmap, index) of roadmapsup" :key="index">
-          <li>
-            <div class="roadmap-date">
-             {{ roadmap.date }}
-            </div>
-            <div class="roadmap-title">
-              {{ roadmap.title }}
-            </div>
-            <div class="roadmap-desc">
-              {{ roadmap.description }}
-            </div>
+          </ul>
+
+            <h1 class="upcom">Upcoming</h1>
+          <ul class="timeline">
+          <li class="timeline" v-for="(roadmap, index) of roadmapsup" :key="index">
+          <div :class="index % 2=== 0 ? 'direction-l' : 'direction-r'">
+            <div class="flag-wrapper">
+        <span class="flag">{{ roadmap.title }}</span>
+        <span class="time-wrapper"><span class="time">{{ roadmap.date }}</span></span>
+        </div>
+        <div class="desc">{{ roadmap.description }}</div>
+        </div>
           </li>
         </ul>
       </div>
     </section>
-	</div>
+  </div>
 </template>
 
 <style>
 
 .roadmap {
-  margin-bottom: 120px;
-  margin-left: 80px;
-  margin-right: 80px;
-  margin-top: 120px;
-}
-.roadmap .roadmap-list {
-  padding-bottom: 16px;
-  border-left: 3px solid #0652DD;
-  padding-left: 24px;
-}
-
-.roadmap .roadmap-list li{
-  position: relative;
-  padding-bottom: 24px;
-  padding-left: 24px;
-}
-
-.roadmap .roadmap-list li::before{
-  content: "";
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  background-color: #0652DD;
-  border: 2px solid #fff;
-  top: 0px;
-  left: -35px;
-  border-radius: 50%;
-}
-
-
-
-.roadmap .roadmap-list li .roadmap-date{
-  font-size: 14px;
-  font-style: italic;
-  color: #888;
-}
-
-.roadmap .roadmap-list li .roadmap-title{
-  font-size: 18px;
-  font-weight: 600;
-  margin: 8px 0;
-}
-
-.roadmap .roadmap-list.upcomming {
-  padding-bottom: 16px;
-  border-left: 3px solid #bdbdbd;
-  padding-left: 24px;
+  margin: 48px auto;
 }
 
 .upcom {
-  padding-top: 36px;
-  padding-bottom: 36px;
-  border-left: 3px solid #bdbdbd;
-  padding-left: 48px;
+  margin: 36px auto;
   font-size: 24px;
   font-weight: 700;
   color: #27ae60;
+  text-align: center;
+  display: block;
 }
 
-.roadmap .roadmap-list.upcomming li::before{
-  background-color: #666666;
+/* ================ The Timeline ================ */
+
+.timeline {
+  position: relative;
+  width: 660px;
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 1em 0;
+  list-style-type: none;
+}
+
+.timeline:before {
+  position: absolute;
+  left: 50%;
+  top: 0;
+  content: ' ';
+  display: block;
+  width: 6px;
+  height: 100%;
+  margin-left: -3px;
+  background: rgb(80,80,80);
+  background: -moz-linear-gradient(top, rgba(80,80,80,0) 0%, rgb(80,80,80) 8%, rgb(80,80,80) 92%, rgba(80,80,80,0) 100%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(30,87,153,1)), color-stop(100%,rgba(125,185,232,1)));
+  background: -webkit-linear-gradient(top, rgba(80,80,80,0) 0%, rgb(80,80,80) 8%, rgb(80,80,80) 92%, rgba(80,80,80,0) 100%);
+  background: -o-linear-gradient(top, rgba(80,80,80,0) 0%, rgb(80,80,80) 8%, rgb(80,80,80) 92%, rgba(80,80,80,0) 100%);
+  background: -ms-linear-gradient(top, rgba(80,80,80,0) 0%, rgb(80,80,80) 8%, rgb(80,80,80) 92%, rgba(80,80,80,0) 100%);
+  background: linear-gradient(to bottom, rgba(80,80,80,0) 0%, rgb(80,80,80) 8%, rgb(80,80,80) 92%, rgba(80,80,80,0) 100%);
+  
+  z-index: 5;
+}
+
+.timeline li {
+  padding: 1em 0;
+}
+
+.timeline li:after {
+  content: "";
+  display: block;
+  height: 0;
+  clear: both;
+  visibility: hidden;
+}
+
+.direction-l {
+  position: relative;
+  width: 300px;
+  float: left;
+  text-align: right;
+}
+
+.direction-r {
+  position: relative;
+  width: 300px;
+  float: right;
+}
+
+.flag-wrapper {
+  position: relative;
+  display: inline-block;
+  
+  text-align: center;
+}
+
+.flag {
+  position: relative;
+  display: inline;
+  background: rgb(248,248,248);
+  padding: 6px 10px;
+  border-radius: 5px;
+  font-weight: 600;
+  text-align: left;
+}
+
+.direction-l .flag {
+  -webkit-box-shadow: -1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
+  -moz-box-shadow: -1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
+  box-shadow: -1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
+}
+
+.direction-r .flag {
+  -webkit-box-shadow: 1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
+  -moz-box-shadow: 1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
+  box-shadow: 1px 1px 1px rgba(0,0,0,0.15), 0 0 1px rgba(0,0,0,0.15);
+}
+
+.direction-l .flag:before,
+.direction-r .flag:before {
+  position: absolute;
+  top: 50%;
+  right: -40px;
+  content: ' ';
+  display: block;
+  width: 20px;
+  height: 20px;
+  margin-top: -10px;
+  background: #fff;
+  border-radius: 10px;
+  border: 4px solid rgb(255,80,80);
+  z-index: 10;
+}
+
+.direction-r .flag:before {
+  left: -40px;
+}
+
+.direction-l .flag:after {
+  content: "";
+  position: absolute;
+  left: 100%;
+  top: 50%;
+  height: 0;
+  width: 0;
+  margin-top: -8px;
+  border: solid transparent;
+  border-left-color: rgb(248,248,248);
+  border-width: 8px;
+  pointer-events: none;
+}
+
+.direction-r .flag:after {
+  content: "";
+  position: absolute;
+  right: 100%;
+  top: 50%;
+  height: 0;
+  width: 0;
+  margin-top: -8px;
+  border: solid transparent;
+  border-right-color: rgb(248,248,248);
+  border-width: 8px;
+  pointer-events: none;
+}
+
+.time-wrapper {
+  display: inline;
+  line-height: 1em;
+  font-size: 11px;
+  color: #777;
+  vertical-align: middle;
+}
+
+.direction-l .time-wrapper {
+  float: left;
+}
+
+.direction-r .time-wrapper {
+  float: right;
+}
+
+.time {
+  display: inline-block;
+  padding: 4px 6px;
+  background: rgb(248,248,248);
+}
+
+.desc {
+  margin: 1em 0.75em 0 0;
+  font-size: 14px;
+  font-style: italic;
+  line-height: 1.5em;
+}
+
+.direction-r .desc {
+  margin: 1em 0 0 0.75em;
+}
+
+/* ================ Timeline Media Queries ================ */
+
+@media screen and (max-width: 660px) {
+
+.timeline {
+   width: 100%;
+  padding: 4em 0 1em 0;
+}
+
+.timeline li {
+  padding: 2em 0;
+}
+
+.direction-l,
+.direction-r {
+  float: none;
+  width: 100%;
+
+  text-align: center;
+}
+
+.flag-wrapper {
+  text-align: center;
+}
+
+.flag {
+  background: rgb(255,255,255);
+  z-index: 15;
+}
+
+.direction-l .flag:before,
+.direction-r .flag:before {
+  position: absolute;
+  top: -30px;
+  left: 51%;
+  content: ' ';
+  display: block;
+  width: 16px;
+  height: 16px;
+  margin-left: -9px;
+  background: #fff;
+  border-radius: 10px;
+  border: 4px solid rgb(255,80,80);
+  z-index: 10;
+}
+
+.direction-l .flag:after,
+.direction-r .flag:after {
+  content: "";
+  position: absolute;
+  left: 50%;
+  top: -8px;
+  height: 0;
+  width: 0;
+  margin-left: -8px;
+  border: solid transparent;
+  border-bottom-color: rgb(255,255,255);
+  border-width: 8px;
+  pointer-events: none;
+}
+
+.time-wrapper {
+  display: block;
+  position: relative;
+  margin: 4px 0 0 0;
+  z-index: 14;
+}
+
+.direction-l .time-wrapper {
+  float: none;
+}
+
+.direction-r .time-wrapper {
+  float: none;
+}
+
+.desc {
+  position: relative;
+  margin: 1em 0 0 0;
+  padding: 1em;
+  background: rgb(245,245,245);
+  -webkit-box-shadow: 0 0 1px rgba(0,0,0,0.20);
+  -moz-box-shadow: 0 0 1px rgba(0,0,0,0.20);
+  box-shadow: 0 0 1px rgba(0,0,0,0.20);
+  
+  z-index: 15;
+}
+
+.direction-l .desc,
+.direction-r .desc {
+  position: relative;
+  margin: 1em 1em 0 1em;
+  padding: 1em;
+  
+  z-index: 15;
+}
+
+}
+
+@media screen and (min-width: 400px ?? max-width: 660px) {
+
+.direction-l .desc,
+.direction-r .desc {
+  margin: 1em 4em 0 4em;
+}
+
 }
 
 </style>
@@ -105,49 +325,49 @@
 
 <script>
 export default {
-	data() {
-		return {
-			roadmaps: [
+  data() {
+    return {
+      roadmaps: [
         {
-					title: 'Protocol Development',
-					date: 'February 15, 2020',
+          title: 'Protocol Development',
+          date: 'February 15, 2020',
           description: 'Started the development of thirm protocol.'
-				},
+        },
 
         {
-					title: 'Token Creation',
-					date: 'May 17, 2020',
+          title: 'Token Creation',
+          date: 'May 17, 2020',
           description: 'Created the THIRM token.'
-				},
+        },
 
         {
-					title: 'Alpha Release',
-					date: 'August 28, 2020',
+          title: 'Alpha Release',
+          date: 'August 28, 2020',
           description: 'Released the Alpha version of the DAPP.'
-				},
+        },
 
         {
-					title: 'Stateofthedapps Listing',
-					date: 'September 6, 2020',
+          title: 'Stateofthedapps Listing',
+          date: 'September 6, 2020',
           description: 'Listed THIRM protocol on "stateofthedapps".'
-				},
+        },
 
         {
-					title: 'Uniswap Listing',
-					date: 'September 27, 2020',
+          title: 'Uniswap Listing',
+          date: 'September 27, 2020',
           description: 'Listed THIRM protocol on "uniswap".'
-				},
-			],
+        },
+      ],
 
       roadmapsup: [
         {
-					title: 'Coming soon',
-					date: 'January 15, 2021',
+          title: 'Coming soon',
+          date: 'January 15, 2021',
           description: 'Coming soon.'
-				},
+        },
 
-			],
-		};
-	},
+      ],
+    };
+  },
 };
 </script>

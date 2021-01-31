fc2ced6e45bcc8b81cc9b3c40fc2ceb867d99b24
Aman Singh <besoeasy@users.noreply.github.com>
2021-01-31 18:52:37 +0530

    Update index.vue

--- src/x/index.vue
+++ src/x/index.vue
@@ -7,13 +7,13 @@
 						<div class="column">
 							<lottie-player src="https://thirm.com/animations/thirm.json" background="transparent" speed="1" style="width: 100%; height: 100%" loop autoplay></lottie-player>
 						</div>
-						<div class="column is-3 is-offset-1">
-							<div class="subtitle"><strong>THIRM PROTOCOL</strong> allows you to transfer your cryptocurrency assets between multiple blockchains. Inter-blockchain access powered by an abstract permissionless VM accessed through open smart contracts on all supported blockchains.</div>
+						<div class="column is-4 is-offset-1">
+							<div class="content box"><strong>THIRM PROTOCOL</strong> allows you to transfer your cryptocurrency assets between multiple blockchains. Inter-blockchain access powered by an abstract permissionless VM accessed through open smart contracts on all supported blockchains.</div>
 
 							<div class="columns is-vcentered">
 								<div class="column">
 									<a href="https://dapp.thirm.com/" target="_blank">
-										<b-button type="is-success is-dark" expanded>Bridge Assets</b-button>
+										<b-button type="is-success is-light" expanded>Bridge Assets</b-button>
 									</a>
 								</div>
 								<div class="column">

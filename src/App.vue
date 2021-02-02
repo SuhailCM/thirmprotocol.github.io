cf4dcc3c4bb2ff8b5b4b983c9d2f88fd27274956
Aman Singh <besoeasy@users.noreply.github.com>
2021-02-02 18:37:11 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,22 @@
 
 				<template slot="end">
 					<b-navbar-item target="_blank" href="https://medium.com/thirmprotocol">
-						<i class="fab fa-medium fa-3x"></i>
+						<i class="fab fa-medium fa-2x"></i>
 					</b-navbar-item>
 					<b-navbar-item target="_blank" href="https://github.com/thirmprotocol">
-						<i class="fab fa-github fa-3x"></i>
+						<i class="fab fa-github fa-2x"></i>
 					</b-navbar-item>
 					<b-navbar-item target="_blank" href="https://twitter.com/thirmprotocol">
-						<i class="fab fa-twitter fa-3x"></i>
+						<i class="fab fa-twitter fa-2x"></i>
 					</b-navbar-item>
 					<b-navbar-item target="_blank" href="https://t.me/thirm">
-						<i class="fab fa-telegram fa-3x"></i>
+						<i class="fab fa-telegram fa-2x"></i>
 					</b-navbar-item>
 					<b-navbar-item target="_blank" href="https://discord.gg/sA8AdfF7ac">
-						<i class="fab fa-discord fa-3x"></i>
+						<i class="fab fa-discord fa-2x"></i>
 					</b-navbar-item>
 					<b-navbar-item target="_blank" href="https://www.linkedin.com/company/thirm/?viewAsMember=true">
-						<i class="fab fa-linkedin fa-3x"></i>
+						<i class="fab fa-linkedin fa-2x"></i>
 					</b-navbar-item>
 				</template>
 			</b-navbar>

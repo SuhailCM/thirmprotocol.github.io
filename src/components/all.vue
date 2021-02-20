7cbf0769a8154a9c8c44e8d3574ad0e618e78f37
Aman Singh <besoeasy@users.noreply.github.com>
2021-02-20 06:00:23 +0530

    Update all.vue

--- src/components/all.vue
+++ src/components/all.vue
@@ -11,11 +11,9 @@
 				<div class="columns is-multiline">
 					<div class="column is-3" v-for="item in lot" :key="item.symbol">
 						<b-tooltip :label="item.name" position="is-top">
-							<a target="_blank" v-bind:href="'https://etherscan.io/address/' + item.address">
-								<figure class="coin-image is-128x128">
-									<img v-bind:src="item.logoURI.trim()" />
-								</figure>
-							</a>
+							<figure class="coin-image is-128x128">
+								<img v-bind:src="item.logoURI.trim()" />
+							</figure>
 						</b-tooltip>
 					</div>
 				</div>
@@ -34,10 +32,10 @@
 }
 
 .b-tooltip {
-    width: 100%;
+	width: 100%;
 }
 
-.coin-image{
+.coin-image {
 	padding: 16px;
 	display: flex;
 	width: 100%;
@@ -45,7 +43,6 @@
 	justify-content: center;
 	align-items: center;
 }
-
 </style>
 
 <script>
@@ -56,25 +53,21 @@ export default {
 				{
 					name: 'Thirm Nano',
 					symbol: 'tNANO',
-					address: '0x9DB44C215F14a874C0Ca45895f3A97f0839c29d9',
 					logoURI: 'https://ipfs.io/ipfs/QmexKGqDqovzHZRZQkHvEv9GoWfi9yfDLah8HNWRrdAbHK/tNANO.png',
 				},
 				{
 					name: 'Thirm Bitcoin',
 					symbol: 'tBTC',
-					address: '0x36910c9695C1286c9377CEbc61D37bDEFcFdeAC6',
 					logoURI: 'https://ipfs.io/ipfs/QmexKGqDqovzHZRZQkHvEv9GoWfi9yfDLah8HNWRrdAbHK/tBTC.png',
 				},
 				{
 					name: 'Thirm Monero (In Development)',
 					symbol: 'tXMR',
-					address: 'xxx',
 					logoURI: 'https://ipfs.io/ipfs/QmWgMfHwJHDAUn9EH5P9cVnRU6PLNNURhYwAMovjKSavwD',
 				},
 				{
 					name: 'Thirm Litecoin (In Development)',
 					symbol: 'tLTC',
-					address: 'xxx',
 					logoURI: 'https://ipfs.io/ipfs/Qme3WEsNjQr3XrV59SUtihpgUErjSCimNPTdCqYDRsCFxm',
 				},
 			],

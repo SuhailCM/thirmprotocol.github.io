33d312273172e667f978dc85164655faf7620e32
SuhailCM <77317232+SuhailCM@users.noreply.github.com>
2021-03-01 21:49:32 +0700

    Update about.vue

--- src/x/about.vue
+++ src/x/about.vue
@@ -24,14 +24,14 @@
 						</div>
 						<h5 class="title is-5">Aman Singh</h5>
 						<p class="subtitle is-6">Founder, Developer</p>
-	<p>.......</p>					</div>
+	<p>Aman Singh brings experience from Bitfinex, Heroku, work experience contributing to NANO and BTC community making various library and modules used by 1000+ projects</p>					</div>
 					<div class="column is-3">
 						<div class="level">
 							<img class="is-rounded" src="https://ipfs.io/ipfs/QmWq9Qp5Yg2Fpxctq4cgH8Ge2FJqDQd5nFnsJAgfz95eQo" alt="" />
 						</div>
 						<h5 class="title is-5">Madhav Poudel</h5>
 						<p class="subtitle is-6">Developer</p>
-						<p>.......</p>
+						<p>Madhav P, most notable contribution comes with developing and debugging brave codebase + Madhav is also known for creating various libraries for UI/UX</p>
 					</div>
 					<div class="column is-3">
 						<div class="level">
@@ -39,14 +39,14 @@
 						</div>
 						<h5 class="title is-5">Suhail L</h5>
 						<p class="subtitle is-6">Manager</p>
-	<p>.......</p>					</div>
+	<p>Suhail L, an entrepenur and a trader who has been previously involved with different projects such as FastSwap brings his vast experience and network to Thirm </p>					</div>
 					<div class="column is-3">
 						<div class="level">
 							<img class="is-rounded" src="https://ipfs.io/ipfs/QmRxVvi1ggkPAptY61iRbRUFW3F6NVHQcLQBRvAtsrs4Jz" alt="" />
 						</div>
 						<h5 class="title is-5">Paul</h5>
 						<p class="subtitle is-6">Tester (Part Time)</p>
-	<p>.......</p>					</div>
+	<p>Paul, a part time employee who tests, tweaks and gives feedback on various modules devloped by Thirm. </p>					</div>
 				</div>
 			</div>
 		</section>

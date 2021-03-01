e10b5318b705d7d27f9f099fb0ef3ea49bd9aa83
Aman Singh <besoeasy@users.noreply.github.com>
2021-03-01 23:00:32 +0530

    Update about.vue

--- src/x/about.vue
+++ src/x/about.vue
@@ -4,49 +4,58 @@
 			<div class="hero-body">
 				<div class="container">
 					<div class="columns is-vcentered">
-						<div class="column is-5">
+						<div class="column is-6">
 							<lottie-player src="https://thirm.com/animations/about.json" background="transparent" speed="1" style="width: 100%; height: 100%" loop autoplay></lottie-player>
 						</div>
 						<div class="column ">
-							<div class="subtitle">THIRM PROTOCOL, THIRM VM, THIRM TOKEN is developed and maintained by THIRM SOLUTIONS, a company operating from INDIA & CAYMAN ISLANDS. We believe that cryptocurrencies should be convenient for everyone. We aim to make crosschain cryptocurrency management a hassle-free, one-click process. We believe that cryptocurrencies should be convenient for everyone.</div>
+							<div class="content box">THIRM PROTOCOL, THIRM VM, THIRM TOKEN is developed and maintained by THIRM SOLUTIONS, a company operating from INDIA & CAYMAN ISLANDS. We believe that cryptocurrencies should be convenient for everyone. We aim to make crosschain cryptocurrency management a hassle-free, one-click process. We believe that cryptocurrencies should be convenient for everyone.</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
 
-		<section class="section is-medium">
-			<div class="container">
-				<div class="columns">
-					<div class="column is-3">
-						<div class="level">
-							<img class="is-rounded" src="https://ipfs.io/ipfs/QmP61RC27XooEZxDU4W48qgFoxXoKP2wC37d7KoAffTvah" alt="" />
+		<section class="hero is-medium is-light">
+			<div class="hero-body">
+				<div class="container">
+					<div class="columns">
+						<div class="column is-3">
+							<div class="level box">
+								<img class="is-rounded" src="https://ipfs.io/ipfs/QmP61RC27XooEZxDU4W48qgFoxXoKP2wC37d7KoAffTvah" alt="" />
+							</div>
+							<div class="box">
+								<h5 class="title is-5">Aman Singh</h5>
+								<p class="subtitle is-6">Founder, Developer</p>
+							</div>
 						</div>
-						<h5 class="title is-5">Aman Singh</h5>
-						<p class="subtitle is-6">Founder, Developer</p>
-	<p>.......</p>					</div>
-					<div class="column is-3">
-						<div class="level">
-							<img class="is-rounded" src="https://ipfs.io/ipfs/QmWq9Qp5Yg2Fpxctq4cgH8Ge2FJqDQd5nFnsJAgfz95eQo" alt="" />
+						<div class="column is-3">
+							<div class="level box">
+								<img class="is-rounded" src="https://ipfs.io/ipfs/QmWq9Qp5Yg2Fpxctq4cgH8Ge2FJqDQd5nFnsJAgfz95eQo" alt="" />
+							</div>
+							<div class="box">
+								<h5 class="title is-5">Madhav Poudel</h5>
+								<p class="subtitle is-6">Developer</p>
+							</div>
 						</div>
-						<h5 class="title is-5">Madhav Poudel</h5>
-						<p class="subtitle is-6">Developer</p>
-						<p>.......</p>
+						<div class="column is-3">
+							<div class="level box">
+								<img class="is-rounded" src="https://ipfs.io/ipfs/QmbnXQuL5z7YnyjGTZDQz7gzGPkphSJSSMdACVy2BFAP5w" alt="" />
+							</div>
+							<div class="box">
+								<h5 class="title is-5">Suhail L</h5>
+								<p class="subtitle is-6">Manager</p>
+							</div>
+						</div>
+						<div class="column is-3">
+							<div class="level box">
+								<img class="is-rounded" src="https://ipfs.io/ipfs/QmRxVvi1ggkPAptY61iRbRUFW3F6NVHQcLQBRvAtsrs4Jz" alt="" />
+							</div>
+							<div class="box">
+								<h5 class="title is-5">Paul</h5>
+								<p class="subtitle is-6">Tester (Part Time)</p>
+							</div>
+						</div>
 					</div>
-					<div class="column is-3">
-						<div class="level">
-							<img class="is-rounded" src="https://ipfs.io/ipfs/QmbnXQuL5z7YnyjGTZDQz7gzGPkphSJSSMdACVy2BFAP5w" alt="" />
-						</div>
-						<h5 class="title is-5">Suhail L</h5>
-						<p class="subtitle is-6">Manager</p>
-	<p>.......</p>					</div>
-					<div class="column is-3">
-						<div class="level">
-							<img class="is-rounded" src="https://ipfs.io/ipfs/QmRxVvi1ggkPAptY61iRbRUFW3F6NVHQcLQBRvAtsrs4Jz" alt="" />
-						</div>
-						<h5 class="title is-5">Paul</h5>
-						<p class="subtitle is-6">Tester (Part Time)</p>
-	<p>.......</p>					</div>
 				</div>
 			</div>
 		</section>
